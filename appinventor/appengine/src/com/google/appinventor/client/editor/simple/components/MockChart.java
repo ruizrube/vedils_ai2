df8c26800dbb82bedd832a6c190a887e44e414a9
Taty Person <tatyperson22@gmail.com>
2016-11-29 03:20:46 +0100

    Ajustando tamaño en pantalla (diseñador) de MockChart y MockDataTable.

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockChart.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockChart.java
@@ -1,7 +1,5 @@
-/**
- * 
- */
 package com.google.appinventor.client.editor.simple.components;
+
 import com.google.appinventor.client.editor.simple.SimpleEditor;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.SimplePanel;
@@ -33,7 +31,7 @@ public class MockChart extends MockVisibleComponent {
 	private final Image lineLargeImage = new Image(images.lineChartbig());
 	private final Image pieLargeImage = new Image(images.pieChartbig());
 		
-	SimplePanel webViewerWidget; 
+	SimplePanel mockChartWidget; 
 	
 	/**
 	 * Creates a new MockActivityGraph component.
@@ -44,57 +42,42 @@ public class MockChart extends MockVisibleComponent {
 	public MockChart(SimpleEditor editor) {
 		super(editor, TYPE, images.chart());
 
-		// Initialize mock WebViewer UI
-		webViewerWidget = new SimplePanel();
-		webViewerWidget.setStylePrimaryName("ode-SimpleMockContainer");
+		// Initialize mock Chart UI
+		mockChartWidget = new SimplePanel();
+		mockChartWidget.setStylePrimaryName("ode-SimpleMockContainer");
 		// TODO(halabelson): Center vertically as well as horizontally
-		webViewerWidget.addStyleDependentName("centerContents");
-		webViewerWidget.setWidget(barLargeImage);
-		webViewerWidget.setHeight("100%");
-		initComponent(webViewerWidget);
+		mockChartWidget.addStyleDependentName("centerContents");
+		mockChartWidget.setWidget(barLargeImage);
+		initComponent(mockChartWidget);
 	}
 
 	private void setChartTypeProperty(String newValue) {
 		
 		if(newValue.equals(BAR + "")){
-			webViewerWidget.setWidget(barLargeImage);
+			mockChartWidget.setWidget(barLargeImage);
 		} else if(newValue.equals(LINE + "")){
-			webViewerWidget.setWidget(lineLargeImage);
+			mockChartWidget.setWidget(lineLargeImage);
 			
 		}
 		// TODO Auto-generated method stub	
 	}
 	
-	// If these are not here, then we don't see the icon as it's
-	// being dragged from the pelette
 	@Override
 	public int getPreferredWidth() {
-		return barLargeImage.getWidth();
+		if(mockChartWidget.getWidget().equals(barLargeImage)) {
+			return barLargeImage.getWidth();
+		} else {
+			return lineLargeImage.getWidth();
+		}
 	}
 
 	@Override
 	public int getPreferredHeight() {
-		return barLargeImage.getHeight();
-	}
-
-	// override the width and height hints, so that automatic will in fact be
-	// fill-parent
-	@Override
-	int getWidthHint() {
-		int widthHint = super.getWidthHint();
-		if (widthHint == LENGTH_PREFERRED) {
-			widthHint = LENGTH_FILL_PARENT;
+		if(mockChartWidget.getWidget().equals(barLargeImage)) {
+			return barLargeImage.getHeight();
+		} else {
+			return lineLargeImage.getHeight();
 		}
-		return widthHint;
-	}
-
-	@Override
-	int getHeightHint() {
-		int heightHint = super.getHeightHint();
-		if (heightHint == LENGTH_PREFERRED) {
-			heightHint = LENGTH_FILL_PARENT;
-		}
-		return heightHint;
 	}
 
 	@Override
@@ -107,8 +90,5 @@ public class MockChart extends MockVisibleComponent {
 		      setChartTypeProperty(newValue);
 		      refreshForm();
 		}
-
 	}
-
-	
 }
